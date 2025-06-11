df7f09f293a8f429b7843d8d82f81140660386fa
Vishnuvardhan-143 <vishnuvardhan5770648@gmail.com>
2025-06-11 19:03:08 +0530

    Update utils.py

--- resume_parser/parser/utils.py
+++ resume_parser/parser/utils.py
@@ -1,10 +1,9 @@
 import re
 
-EMAIL_REGEX = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
-PHONE_REGEX = r'(\+?\d{1,3})?[\s-]?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}'
-
 def extract_emails(text):
-    return re.findall(EMAIL_REGEX, text)
+    email_pattern = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
+    return re.findall(email_pattern, text)
 
 def extract_phone_numbers(text):
-    return re.findall(PHONE_REGEX, text)
+    phone_pattern = r'(\+\d{1,3}[\s-]?)?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}'
+    return re.findall(phone_pattern, text)
