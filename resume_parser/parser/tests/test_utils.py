f9446f8ccfda5c1ad73937b73bee8da78204b557
Vishnuvardhan-143 <vishnuvardhan5770648@gmail.com>
2025-06-11 19:04:05 +0530

    Update test_utils.py

--- resume_parser/parser/tests/test_utils.py
+++ resume_parser/parser/tests/test_utils.py
@@ -1,9 +1,9 @@
 from parser import utils
 
 def test_extract_emails():
-    text = "My email is test.user@example.com"
-    assert utils.extract_emails(text) == ["test.user@example.com"]
+    text = "Contact: abc@example.com"
+    assert utils.extract_emails(text) == ["abc@example.com"]
 
 def test_extract_phone_numbers():
     text = "Call me at +1 123-456-7890"
-    assert utils.extract_phone_numbers(text) == [('+1',)]
+    assert utils.extract_phone_numbers(text) == ['+1 ']
